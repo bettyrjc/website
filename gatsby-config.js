578dedaafb8e693e83b9512291388b6a7e6f979e
Daniel Esteves <estevesd8@gmail.com>
2020-08-30 22:57:18 -0400

    🔨 feature: purge options for Tailwind

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,8 +9,8 @@ module.exports = {
       resolve: `gatsby-plugin-sass`,
       options: {
         postCssPlugins: [
-          require("tailwindcss"),
-          require("./tailwind.config.js"), // Optional: Load custom Tailwind CSS configuration
+          require(`tailwindcss`),
+          require(`./tailwind.config.js`), // Optional: Load custom Tailwind CSS configuration
         ],
       },
     },
